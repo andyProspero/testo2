0347abd21c5a7502e647565adaef927ff78e0a8a
Andrey Egorov <a.egorov@agentura-soft.ru>
2022-09-24 14:16:40 +0300

    added bs

--- test2.php
+++ test2.php
@@ -1,2 +1,17 @@
-<?
+<?php
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+
+// подключаем bootstrap (v. 5.0.0)
+use Bitrix\Main\UI\Extension;
+Extension::load('ui.bootstrap4');
+
+$APPLICATION->SetTitle("Testovoe 2");
+
+
 // testovoe 2
+
+
+
+
+
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");
